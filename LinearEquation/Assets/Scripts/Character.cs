20975c78b1ccd5c34a0be4f03543f5e225e46e8d
JeonChangMin <wisechang1@naver.com>
2018-10-25 15:03:36 +0900

    한자리 숫자인 경우 구현

--- LinearEquation/Assets/Scripts/Character.cs
+++ LinearEquation/Assets/Scripts/Character.cs
@@ -5,17 +5,81 @@ using UnityEngine;
 public class Character : MonoBehaviour {
     public GameObject EventController;
 
+    private GameObject Collision;   // 부딫히는 중인 GameObject
     private EventController ec;
+    private bool isOut;             // Out과 충돌 중인지 확인
+    private int ChildCount;         // Children 수, 0 ~ 2
 
     void Awake()
     {
         ec = EventController.GetComponent<EventController>();
+        isOut = true;
+        ChildCount = 0;
     }
 
-    public int GetItem()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        // if(collider 부딫히면) 태그보고 처리
-        // 아이템을 줍는 경우와 Out에 갖다놓는 경우 처리
-        return -1;
+        if (collision.transform.tag == "out")
+            isOut = true;
+        Collision = collision.transform.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "out")
+            isOut = false;
+        if (Collision != null && 
+            Collision.transform.name == collision.transform.name) Collision = null;
+    }
+
+    public void SpaceInput()
+    {
+        ChildCount = transform.childCount;
+        if (isOut && ChildCount == 2) // Out에 갖다놓는 경우
+        {
+            if (transform.GetChild(0).tag == "num")
+            {
+                ec.GetNum = (int)System.Char.GetNumericValue(transform.GetChild(0).name[0]);
+                ec.GetOper = transform.GetChild(1).tag;
+            } else
+            {
+                ec.GetNum = (int)System.Char.GetNumericValue(transform.GetChild(1).name[0]);
+                ec.GetOper = transform.GetChild(0).tag;
+            }
+            for (int i = 1; i >=0; i--) Destroy(transform.GetChild(i).gameObject);
+            if (!ec.isOperated)
+            {
+                ec.Operate();
+                ec.isOperated = true;
+            }
+            else ec.isOperated = false;
+        }
+        else if (Collision == null)   // Item 버리는 경우
+        {
+            if (ChildCount != 0)
+                transform.GetChild(ChildCount - 1).transform.parent = ec.ItemsPar.transform;
+        }
+        else if (Collision.tag != "out") // Item 줍는 경우
+        {
+            if (ChildCount == 0 || (ChildCount == 1 && transform.GetChild(0).tag != Collision.transform.tag)) {
+                Collision.transform.parent = transform;
+            }
+            else if (ChildCount == 1 && transform.GetChild(0).transform.tag == Collision.transform.tag)
+            {
+                transform.GetChild(0).parent = ec.ItemsPar.transform;
+                Collision.transform.parent = transform;
+            }
+            else if (ChildCount == 2)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    if (transform.GetChild(i).tag == Collision.transform.tag)
+                    {
+                        transform.GetChild(i).parent = ec.ItemsPar.transform;
+                        Collision.transform.parent = transform;
+                    }
+                }
+            }
+        }
     }
 }
--- LinearEquation/Assets/Scripts/EventController.cs
+++ LinearEquation/Assets/Scripts/EventController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EventController : MonoBehaviour {
+    public void Debugging()
+    {
+        Debug.Log(Coefficient.ToString() + " " + Constant.ToString() + " " + Result.ToString());
+        Debug.Log(Coefficient_Deno.ToString() + " " + Constant_Deno.ToString() + " " + Result_Deno.ToString());
+    }
     // 케릭터들
     public GameObject LeftCharacter;
     public GameObject RightCharacter;
@@ -14,32 +19,53 @@ public class EventController : MonoBehaviour {
     public GameObject x;                                // x
 
     // 주워쓸 아이템
-    public GameObject[] NumberItems = new GameObject[10];
-    public GameObject[] OperItems = new GameObject[3]; // +, - , /
+    public GameObject[] Items = new GameObject[15]; // 0 ~ 9: 숫자, 10, 11, 12, 13: +, -, /, *,   14: Empty
+    private static int max_itemKind = 15;
 
     // Prefab 부모 Objects
+    public GameObject EquationPar;
     public GameObject CoefficientPar;
     public GameObject ConstantPar;
     public GameObject ResultPar;
     public GameObject ItemsPar;
-    public GameObject OperBoxPar;
-    public GameObject NumBoxPar;
+
+    // Item 정보
+    private int[] ItemsData = new int[10]; // -1: 빔,   0 ~ 9: 숫자,   10, 11, 12, 13: +, -, /, *
+    private Vector3[] ItemsPos = new Vector3[10];
+    private static int max_itemNum = 10;
+
+    // Random Class
+    private System.Random rand;
 
     // 일차방정식 x계수, 상수항, 우변의 결과(분자, 분모), 우변의 결과가 분수인지 아닌지
-    private int Coefficient;        // x의 계수 (1~20)
+    private int Coefficient;        // x의 계수 (1~10)
     private int Coefficient_Deno;   // x의 계수 분모\
-    private int Constant;           // 좌변의 상수항 (-100~100) (2x+5=0일 때 +값을 가짐)
+    private int Constant;           // 좌변의 상수항 (-9~9) (2x+5=0일 때 +값을 가짐)
     private int Constant_Deno;      // 좌변의 상수항 분모\
     private int Result;             // 결과의 분자
-    private int Result_Deno;           // 결과의 분모\
+    private int Result_Deno;        // 결과의 분모\
 
     // 주운 연산자, 숫자 저장
-    private int GetOper;    // -1, 0, 1, 2: 안주움, +, -, /
-    private int GetNum;     // -1: 안주움
+    public string GetOper; // "", "plus", "minus", "div", "mul": 안주움, +, -, /, *
+    public int GetNum;     // -1: 안주움
+
+    // LeftCharacter, RightCharacter 중복으로 Operate 방지
+    public bool isOperated;
     
 ///////////////////////////////////////////////////////////////////////////// Init
     void Awake()
     {
+        rand = new System.Random();
+        ItemsPos[0] = new Vector3(-6.81f, -3.82f);
+        ItemsPos[1] = new Vector3(-7.48f, 0.0f);
+        ItemsPos[2] = new Vector3(-0.5f, -4.38f);
+        ItemsPos[3] = new Vector3(-0.76f, 0.0f);
+        ItemsPos[4] = new Vector3(-1.8f, -0.79f);
+        ItemsPos[5] = new Vector3(-2.33f, -2.3f);
+        ItemsPos[6] = new Vector3(-2.94f, -1.2f);
+        ItemsPos[7] = new Vector3(-7.33f, -1.83f);
+        ItemsPos[8] = new Vector3(-4.89f, -1.89f);
+        ItemsPos[9] = new Vector3(-0.35f, -1.7f);
         Restart();
     }
 
@@ -51,26 +77,134 @@ public class EventController : MonoBehaviour {
 
         NumbersInit();  // 일차방정식 숫자 세팅 초기화
         ItemsInit();    // 떨어져있는 아이템 세팅 초기화
+        DisplayEquation();
     }
 
     void NumbersInit()  // 일차방정식 숫자 랜덤 생성 및 기타 숫자들 초기화
     {
-        System.Random rand = new System.Random();
-        Coefficient = rand.Next(1, 21);
+        Coefficient = rand.Next(1, 10);
         Coefficient_Deno = 1;
-        Constant = rand.Next(-100, 101);
+        Constant = rand.Next(-9, 10);
         Constant_Deno = 1;
         Result = 0;
         Result_Deno = 1;
-        GetOper = -1;
+        GetOper = "";
         GetNum = -1;
+        isOperated = false;
     }
 
-    void ItemsInit()    // 아이템 초기화, 생성된 숫자에 알맞게 + 위치 랜덤으로 아이템 나와야하는데 아직 구현X
+    void ItemsInit()    // 아이템 초기화, 생성된 숫자, 연산자에 알맞게 + 위치 랜덤
     {
-
+        RemoveItems();
+        for (int i = 0; i < max_itemNum; i++) ItemsData[i] = -1;
+        int rndCoe = rand.Next(0, 10);
+        ItemsData[rndCoe] = Coefficient;
+        int rndCon = rand.Next(0, 9);
+        for (int i = rndCon;; i++)
+        {
+            if (i == max_itemNum) i = 0;
+            if (ItemsData[i] == -1)
+            {
+                if (Constant < 0) ItemsData[i] = -Constant;
+                else ItemsData[i] = Constant;
+                break;
+            }
+        }
+        if (Coefficient_Deno != 1)
+        {
+            int rndCoe_Deno = rand.Next(0, 8);
+            for (int i = rndCoe_Deno;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = Coefficient_Deno;
+                    break;
+                }
+            }
+        }
+        if(Constant != 0 && Constant_Deno != 1)
+        {
+            int rndCon_Deno = rand.Next(0, 7);
+            for (int i = rndCon_Deno;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = Constant_Deno;
+                    break;
+                }
+            }
+        }
+        if (Constant < 0)
+        {
+            int rndPlus = rand.Next(0, 6);
+            for (int i = rndPlus;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = 10;  // Plus
+                    break;
+                }
+            }
+        } else
+        {
+            int rndMinus = rand.Next(0, 5);
+            for (int i = rndMinus;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = 11;  // Minus
+                    break;
+                }
+            }
+        }
+        if (Coefficient > Coefficient_Deno)
+        {
+            int rndDiv = rand.Next(0, 4);
+            for (int i = rndDiv;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = 12;  // Div
+                    break;
+                }
+            }
+        }
+        if (Coefficient < Coefficient_Deno)
+        {
+            int rndMul = rand.Next(0, 3);
+            for (int i = rndMul;; i++)
+            {
+                if (i == max_itemNum) i = 0;
+                if (ItemsData[i] == -1)
+                {
+                    ItemsData[i] = 13;  // Mul
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < max_itemNum; i++)
+        {
+            if (ItemsData[i] == -1)
+                ItemsData[i] = rand.Next(0, max_itemKind-1);
+        }
+        for (int i = 0; i < max_itemNum; i++)
+        {
+            Vector3 delta = new Vector3(8f, 0f);
+            Instantiate(Items[ItemsData[i]], ItemsPos[i], new Quaternion(0f, 0f, 0f, 1f), ItemsPar.transform);
+            Instantiate(Items[ItemsData[i]], ItemsPos[i]+delta, new Quaternion(0f, 0f, 0f, 1f), ItemsPar.transform);
+        }
     }
 
+    void RemoveItems()
+    {
+        int ItemsNum = ItemsPar.transform.childCount;
+        for (int i = ItemsNum - 1; i >= 0; i--) Destroy(ItemsPar.transform.GetChild(i).gameObject);
+    }
 ///////////////////////////////////////////////////////////////////////////// Update
     void Update () {
         // 캐릭터 움직임 (왼, 오, 위, 아래 순)
@@ -84,8 +218,11 @@ public class EventController : MonoBehaviour {
             Move(LeftCharacter, RightCharacter, 3);
 
         // Space바 눌렀을 때
-        if (Input.GetKey("space"))
-            SpaceInput();
+        if (Input.GetKeyDown("space"))
+        {
+            LeftCharacter.GetComponent<Character>().SpaceInput();
+            RightCharacter.GetComponent<Character>().SpaceInput();
+        }
     }
 
     // 캐릭터 움직임
@@ -117,49 +254,52 @@ public class EventController : MonoBehaviour {
         RightCharacter.transform.localPosition = LeftCharacter.transform.localPosition + delta;
     }
 
-    // SpaceBar 눌렀을 때
-    void SpaceInput()
-    {
-        KeepItem();                         // 아이템 줍고
-        if (GetOper != -1 && GetNum != -1)  // Operation 필요시 Operate();
-            Operate();                      // 여기서 많은걸 컨트롤
-    }
-
-    void KeepItem() // 아이템 줍기
-    {   // 주운 아이템 뭔지 저장
-        GetOper = LeftCharacter.GetComponent<Character>().GetItem();
-        GetNum = RightCharacter.GetComponent<Character>().GetItem();
-        DisplayNumbers(NumBoxPar, false);
-        // Instanciate로 OperBox 안에 Operator 생성
-    }
-
-    void Operate()  // 연산 및 Display하는 함수
+    public void Operate()  // 연산 및 Display하는 함수
     {               // Display는 계수, 상수항, 결과값의 숫자 Display는 통일
-        switch(GetOper)
+        int CoefGCD;
+        int ConstGCD;
+        int ResGCD;
+        switch (GetOper)
         {
-            case 0: // + 덧셈
+            case "plus": // + 덧셈
                 Constant += Constant_Deno * GetNum;
                 Result += Result_Deno * GetNum;
                 break;
-            case 1: // - 뺄셈
+            case "minus": // - 뺄셈
                 Constant -= Constant_Deno * GetNum;
                 Result -= Result_Deno * GetNum;
                 break;
-            case 2: // / 나눗셈
+            case "div": // / 나눗셈
                 Coefficient_Deno *= GetNum;
                 Constant_Deno *= GetNum;
                 Result_Deno *= GetNum;
-                int CoefGCD = gcd(Coefficient, Coefficient_Deno);
-                int ConstGCD = gcd(Constant, Constant_Deno);
-                int ResGCD = gcd(Result, Result_Deno);
+                CoefGCD = gcd(Coefficient, Coefficient_Deno);
+                ConstGCD = gcd(Constant, Constant_Deno);
+                ResGCD = gcd(Result, Result_Deno);
                 Coefficient /= CoefGCD;
                 Coefficient_Deno /= CoefGCD;
                 Constant /= ConstGCD;
                 Constant_Deno /= ConstGCD;
                 Result /= ResGCD;
-                Result_Deno = ResGCD;
+                Result_Deno /= ResGCD;
+                break;
+            case "mul": // * 곱셈
+                Coefficient *= GetNum;
+                Constant *= GetNum;
+                Result *= GetNum;
+                CoefGCD = gcd(Coefficient, Coefficient_Deno);
+                ConstGCD = gcd(Constant, Constant_Deno);
+                ResGCD = gcd(Result, Result_Deno);
+                Coefficient /= CoefGCD;
+                Coefficient_Deno /= CoefGCD;
+                Constant /= ConstGCD;
+                Constant_Deno /= ConstGCD;
+                Result /= ResGCD;
+                Result_Deno /= ResGCD;
                 break;
         }
+
+        RemoveEquation();
 
         if (Coefficient == Coefficient_Deno && Constant == 0)  // 연산 결과로 일차방정식 해를 구했으면
             Restart();                                         // 재시작
@@ -167,32 +307,83 @@ public class EventController : MonoBehaviour {
             DisplayEquation();
     }
 
-    void DisplayEquation()  // 상태에 맞게 Display 변환
+    void RemoveEquation()   // 계수, 연산자, 상수항, 결과식 Prefab Destroy
     {
+        int CoefNum = CoefficientPar.transform.childCount;
+        int ConstNum = ConstantPar.transform.childCount;
+        int ResNum = ResultPar.transform.childCount;
 
+        if (EquationPar.transform.childCount == 4) Destroy(EquationPar.transform.GetChild(3).gameObject);
+        for (int i = CoefNum - 1; i >= 0; i--) Destroy(CoefficientPar.transform.GetChild(i).gameObject);
+        for (int i = ConstNum - 1; i >= 0; i--) Destroy(ConstantPar.transform.GetChild(i).gameObject);
+        for (int i = ResNum - 1; i >= 0; i--) Destroy(ResultPar.transform.GetChild(i).gameObject);
     }
 
-    void DisplayNumbers(GameObject Par, bool isFraction)  // 모든 숫자들 Display 컨트롤, 분수도 처리
+    void DisplayEquation()  // 상태에 맞게 Display 변환
     {
-        if (!isFraction)    // 분수 아닌경우
+        if (Coefficient != Coefficient_Deno && Constant != 0)       // 계수, 상수항 모두 있는 경우
         {
-
+            DisplayNumbers(Coefficient, Coefficient_Deno, CoefficientPar, new Vector3(-6.24f, 2.84f));
+            x.transform.localPosition = new Vector3(-5.05f, 2.84f);
+            DisplayOperator(Constant < 0, EquationPar, new Vector3(-3.49f, 2.84f));
+            DisplayNumbers(abs(Constant), Constant_Deno, ConstantPar, new Vector3(-1.8f, 2.84f));
+        } else if(Coefficient != Coefficient_Deno && Constant == 0) // 계수만 있는 경우
+        {
+            DisplayNumbers(Coefficient, Coefficient_Deno, CoefficientPar, new Vector3(-5.01f, 2.84f));
+            x.transform.localPosition = new Vector3(-3.44f, 2.84f);
+        } else if(Coefficient == Coefficient_Deno && Constant != 0) // 상수항만 있는 경우
+        {
+            x.transform.localPosition = new Vector3(-6.15f, 2.84f);
+            DisplayOperator(Constant < 0, EquationPar, new Vector3(-4.12f, 2.84f));
+            DisplayNumbers(abs(Constant), Constant_Deno, ConstantPar, new Vector3(-2.27f, 2.84f));
         }
+        if (Result < 0)
+        {
+            DisplayOperator(Result < 0, EquationPar, new Vector3(3.78f, 2.84f));
+            DisplayNumbers(-Result, Result_Deno, ResultPar, new Vector3(4.8f, 2.84f));
+        }
+        else DisplayNumbers(Result, Result_Deno, ResultPar, new Vector3(4.03f, 2.84f));
+    }
+
+    void DisplayNumbers(int num, int deno, GameObject par, Vector3 pos)  // 모든 숫자들 Display 컨트롤, 분수도 처리
+    {
+        // Debug.Log(num.ToString() + " " + deno.ToString());
+        if (deno == 1)    // 분수 아닌경우
+            Instantiate(Numbers[num], pos, new Quaternion(0f, 0f, 0f, 1f), par.transform);
         else                // 분수인 경우
         {
-                    
+            Vector3 numerator = new Vector3(0f, 0.76f);
+            Vector3 denominator = new Vector3(0f, -1.26f);
+            Instantiate(Numbers[num], pos + numerator, new Quaternion(0f, 0f, 0f, 1f), par.transform);
+            Instantiate(FractionBar, pos, new Quaternion(0f, 0f, 0f, 1f), par.transform);
+            Instantiate(Numbers[deno], pos + denominator, new Quaternion(0f, 0f, 0f, 1f), par.transform);
         }
+    }
+
+    void DisplayOperator(bool isMinus, GameObject par, Vector3 pos)
+    {
+        if (isMinus) Instantiate(Opers[1], pos, new Quaternion(0f, 0f, 0f, 1f), par.transform);
+        else Instantiate(Opers[0], pos, new Quaternion(0f, 0f, 0f, 1f), par.transform);
     }
 
     int gcd(int a, int b)
     {
+        a = abs(a);
+        b = abs(b);
+        if (a == 0) return b;
         if (a < b)
         {
             int tmp = a;
             a = b;
             b = tmp;
         }
-        if (b == 1) return 1;
+        if (a % b == 0) return b;
         return gcd(b, a % b);
+    }
+
+    int abs(int a)
+    {
+        if (a < 0) return -a;
+        return a;
     }
 }
