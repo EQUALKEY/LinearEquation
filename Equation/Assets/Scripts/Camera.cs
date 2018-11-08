d44ce08274e9b5493bf7cf7c5c8ad16cafdd3c56
JeonChangMin <wisechang1@naver.com>
2018-11-08 14:21:58 +0900

    Real Version Init

---
+++ Equation/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject Character;
+    private Vector3 Direction;
+    private float Velocity;
+
+    void Awake()
+    {
+        Velocity = 5f;
+    }
+
+	void Update () {
+        Direction = transform.position - Character.transform.position;
+        if(transform.position.z < 0) transform.Translate(new Vector3(-Direction.x * Velocity, -Direction.y * Velocity, 0f) * Time.deltaTime);
+        else  transform.Translate(new Vector3(Direction.x * Velocity, -Direction.y * Velocity, 0f) * Time.deltaTime);
+    }
+}
---
+++ Equation/Assets/Scripts/EventController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventController : MonoBehaviour {
+    public GameObject Character;
+    private float CharacterAngularVelocity;
+    private float Force;
+    private float VelocityLimit;
+    private Vector2 CharacterVelocity;
+    public Text Text;
+
+    void Awake()
+    {
+        VelocityLimit = 3f;
+        CharacterVelocity = new Vector2();
+        CharacterAngularVelocity = 500f;
+        Force = 100f * Character.GetComponent<Rigidbody2D>().mass;
+    }
+
+    void Update() {
+        // 캐릭터 이동 및 속도 조절. 건드리지 말자
+        CharacterVelocity = Character.GetComponent<Rigidbody2D>().velocity;
+        Text.text = CharacterVelocity.magnitude.ToString();
+        float Cross = CharacterVelocity.y * Character.transform.up.y + CharacterVelocity.x * Character.transform.up.x;
+        if (Input.GetKey(KeyCode.LeftArrow)) Character.transform.rotation *= Quaternion.Euler(Vector3.forward * CharacterAngularVelocity * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow)) Character.transform.rotation *= Quaternion.Euler(-Vector3.forward * CharacterAngularVelocity * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow) && (CharacterVelocity.magnitude <= VelocityLimit || Cross < 0)) Character.GetComponent<Rigidbody2D>().AddForce(Character.transform.up * Force * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow) && (CharacterVelocity.magnitude <= VelocityLimit || Cross > 0)) Character.GetComponent<Rigidbody2D>().AddForce(-Character.transform.up * Force * Time.deltaTime);
+    }
+}
